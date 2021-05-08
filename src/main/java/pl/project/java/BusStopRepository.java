973e41b341ad2a1e9073c41785df0e673f817234
daniel <danielpalak11@wp.pl>
2021-05-08 14:07:34 +0200

    new version

--- src/main/java/pl/project/java/BusStopRepository.java
+++ src/main/java/pl/project/java/BusStopRepository.java
@@ -7,8 +7,11 @@ import javax.persistence.Persistence;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
+ * @author Daniel Palak
  * Klasa BusStopRepository, ktora dziedziczy po klasie BusRepository
- * Pole statyczne, prywatne EntityManagerFactory factory tworzy
+ * Pole statyczne, prywatne EntityManagerFactory factory, ktoremu przypisane jest stworzenie EntityManagerFactory dla nazwy bazy danych BusBase
+ * Pole statyczne, prywatne EntityManager theManager, ktoremu przypisane jest stworzenie EntityManagera
+ * Pole statyczne, prywatne EntityTransaction transaction, ktoremu przypisane jest stworzenie menadzera transakcji
  */
 public class BusStopRepository extends BusRepository {
 
@@ -16,10 +19,20 @@ public class BusStopRepository extends BusRepository {
     private static final EntityManager theManager = factory.createEntityManager();
     private static final EntityTransaction transaction = theManager.getTransaction();
 
+    /**
+     * Konstruktor klasy BusStopRepository, któremu wywoływana jest konstrukcja super dzieki ktorej wywolujemy konstruktor klasy nadrzednej
+     * @throws UnsupportedAudioFileException wyrzucenie wyjatku gdy mamy do czynienia z plikiem audio, ktorego format nie jest wspierany
+     */
     public BusStopRepository() throws UnsupportedAudioFileException {
         super();
     }
 
+    /**
+     * Metoda addNewBusStop odpowiada za dodanie do bazy danych obiektow jako przystankow
+     * transaction.begin() rozpoczyna mozliwosc dodania do bazy danych danych
+     * za pomoca theManager.persist() dodajemy obiekt do bazy danych wpisujac jako parametr metody persist() nazwe obiektu. Robimy to dla 140 przystankow
+     * po skonczeniu dodawania obiektow do bazy danych wywolujemy metode commit(), aby zatwierdzic dodane dane
+     */
     public void addNewBusStop()
     {
         transaction.begin();
