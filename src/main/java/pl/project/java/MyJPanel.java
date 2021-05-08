7f4af7d883b45c46cda0965bf9c34a2e10f343af
daniel <danielpalak11@wp.pl>
2021-05-08 14:41:45 +0200

    new version

--- src/main/java/pl/project/java/MyJPanel.java
+++ src/main/java/pl/project/java/MyJPanel.java
@@ -3,8 +3,17 @@ package pl.project.java;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ *  Klasa MyJPanel dziedzicza po klasie JPanel umozliwiajaca ustawienie tla panelu jako dowolny obrazek umieszczony w zasobach projektu
+ */
 public class MyJPanel extends JPanel
 {
+    /**
+     * Adnotacja @Override oznacza nadpisanie metody z klasy macierzystej
+     * Metoda paintComponent wypelnia zawartosc panelu
+     * @param g oznacza to czym zostanie wypelniony panel
+     * Tworzony jest obiekt klasy image, ktoremu przypisujemy obrazek, ktory chcemy w tle panelu podajac sciezke do niego, a nastepnie przekazujemy go do rysowania
+     */
     @Override
     protected void paintComponent(Graphics g)
     {
