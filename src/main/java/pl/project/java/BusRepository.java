07d0e792741e6038e3afd6898222cbe13ebeb8fb
daniel <danielpalak11@wp.pl>
2021-05-08 14:30:03 +0200

    new version

--- src/main/java/pl/project/java/BusRepository.java
+++ src/main/java/pl/project/java/BusRepository.java
@@ -6,16 +6,33 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+/**
+ * @author Daniel Palak
+ * Klasa BusRepository, ktora dziedziczy po klasie Gui
+ * Pole statyczne, prywatne EntityManagerFactory factory, ktoremu przypisane jest stworzenie EntityManagerFactory dla nazwy bazy danych BusBase
+ * Pole statyczne, prywatne EntityManager theManager, ktoremu przypisane jest stworzenie EntityManagera
+ * Pole statyczne, prywatne EntityTransaction transaction, ktoremu przypisane jest stworzenie menadzera transakcji
+ */
 public class BusRepository extends Gui
 {
     private static final EntityManagerFactory factory = Persistence.createEntityManagerFactory("BusBase");
     private static final EntityManager theManager = factory.createEntityManager();
     private static final EntityTransaction transaction = theManager.getTransaction();
 
+    /**
+     * Konstruktor klasy BusRepository, któremu wywoływana jest konstrukcja super dzieki ktorej wywolujemy konstruktor klasy nadrzednej
+     * @throws UnsupportedAudioFileException wyrzucenie wyjatku gdy mamy do czynienia z plikiem audio, ktorego format nie jest wspierany
+     */
     public BusRepository() throws UnsupportedAudioFileException {
         super();
     }
 
+    /**
+     * Metoda addNewBus odpowiada za dodanie do bazy danych obiektow jako autobusow
+     * transaction.begin() rozpoczyna mozliwosc dodania do bazy danych danych
+     * za pomoca theManager.persist() dodajemy obiekt do bazy danych wpisujac jako parametr metody persist() nazwe obiektu. Robimy to dla 10 autobusow
+     * po skonczeniu dodawania obiektow do bazy danych wywolujemy metode commit(), aby zatwierdzic dodane dane
+     */
     public void addNewBus()
     {
         transaction.begin();
